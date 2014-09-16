408fe185370e7391bf8ea785fbc34f8e068146f3
Iker Ibarguren <ikerib@gmail.com>
2014-09-16 10:35:01 +0200

    Fix Egutegia

--- routes/get.js
+++ routes/get.js
@@ -54,7 +54,6 @@ exports.getlinea1 = function(req,res) {
                         var res = req.end();
 
                         if ( (res.data.toString() !== "") && (res.data.toString()!== "undefinded") ) {
-                            console.log(res.data.toString());
                             var miresp = res.data.toString();
                             var mijson = JSON.parse(miresp);
                             if ( mijson.length === 0 ) {
@@ -64,7 +63,6 @@ exports.getlinea1 = function(req,res) {
                                 mijson.forEach(function(entry) {
                                     if ( entry.QPendiente < entry.QNecesaria ) {
                                         orden.badutstock = 1;
-                                        console.log(entry);
                                     } else {
                                         orden.badutstock = 0;
                                     }
@@ -132,7 +130,6 @@ exports.getlinea2 = function(req,res) {
                         var res = req.end();
 
                         if ( (res.data.toString() !== "") && (res.data.toString()!== "undefinded") ) {
-                            //console.log(res.data.toString());
                             var miresp = res.data.toString();
                             var mijson = JSON.parse(miresp);
                             if ( mijson.length === 0 ) {
@@ -142,7 +139,6 @@ exports.getlinea2 = function(req,res) {
                                 mijson.forEach(function(entry) {
                                     if ( entry.QPendiente < entry.QNecesaria ) {
                                         orden.badutstock = 1;
-                                        console.log(entry);
                                     } else {
                                         orden.badutstock = 0;
                                     }
@@ -211,7 +207,6 @@ exports.getlinea3 = function(req,res) {
                         var res = req.end();
 
                         if ( (res.data.toString() !== "") && (res.data.toString()!== "undefinded") ) {
-                            console.log(res.data.toString());
                             var miresp = res.data.toString();
                             var mijson = JSON.parse(miresp);
                             if ( mijson.length === 0 ) {
@@ -221,7 +216,6 @@ exports.getlinea3 = function(req,res) {
                                 mijson.forEach(function(entry) {
                                     if ( entry.QPendiente < entry.QNecesaria ) {
                                         orden.badutstock = 1;
-                                        console.log(entry);
                                     } else {
                                         orden.badutstock = 0;
                                     }
@@ -283,59 +277,29 @@ exports.sartu = function (req, res) {
 
 exports.egutegia = function(req, res){
 
-    if (!db.serverConfig.isConnected()) {
-        db.open(function(err, db) {
-            if(!err) {
-
-            } else {
-                onErr(err, function(){
-                    console.log(err);
-                    db.close();
-                });
-            }
-        });
-    }
 
     var miresp = [];
 
     desde = moment(req.query.start, "YYYY-MM-DD").toISOString();
     hasta = moment(req.query.end, "YYYY-MM-DD").toISOString();
 
-    db.collection('planificacion').find( {
-        $and: [
-            { "fetxa": { $gte: new Date(desde) , $lte: new Date(hasta)  }}
-        ]
-    }).toArray(function(err, items){
-        if(!err) {
+    c_planificacion.find(
+            { "fetxa": { $gte: new Date(desde) , $lte: new Date(hasta)  }
+    },function(err, items){
 
-            for (var i=0; i < items.length; i++ ) {
-                if ( items[i].turnoak.length > 0 ) {
-                    var temp = items[i].turnoak;
-                    for ( var j=0; j < temp.length; j++) {
-
-                        if ( temp[j].ordenes.length > 0) {
-                            var mit = temp[j].ordenes;
-                            for ( var k=0; k < mit.length; k++) {
-                                var miobj = {};
-                                miobj.title = mit[k].ref;
-                                miobj.start = moment(items[i].fetxa).format('YYYY-MM-DD');
-                                miresp.push(miobj);
-                            }
-                        }
-
-                    }
-
-                }
-            }
-
-            res.json(miresp);
-
-        } else {
-            onErr(err, function(){
-                console.log(err);
-                db.close();
-            });
+        if (err) {
+            res.json(500, err);
         }
+
+        forEach (items, function(orden, callback){
+            var miobj = {};
+            miobj.title = orden.ref;
+            miobj.start = moment(orden.fetxa).format('YYYY-MM-DD');
+            miresp.push(miobj);
+        });
+
+        res.json(miresp);
+
     })
 };
 
@@ -358,7 +322,6 @@ exports.getsettings = function(req, res){
             res.json(items);
         } else {
             onErr(err, function(){
-                console.log(err);
             });
         }
     })
@@ -388,7 +351,7 @@ exports.updateSetting = function(req, res){
         backcolor: data.backcolor,
         forecolor: data.forecolor
     } }, {safe:true, multi:false, upsert:false}, function(e, result){
-        if (e) console.log(e)
+//        if (e) console.log(e)
         res.send((result===1)?{msg:'success'}:{msg:'error'+e})
     })
 };
