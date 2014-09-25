c16df62d6c11b36879de6584ffd3683ff95ca3ae
Iker Ibarguren <ikerib@gmail.com>
2014-09-25 15:23:35 +0200

    Egutegia administrariak soilik aldatzea ahalbideratu.

--- static/js/controller/egutegia.js
+++ static/js/controller/egutegia.js
@@ -2,48 +2,63 @@
  * Created by ikerib on 30/06/14.
  */
 
-produccionApp.controller('egutegiaController', function ($scope, $http, socket) {
+produccionApp.controller('egutegiaController', function ($scope, $http, socket, $cookieStore) {
+    if ( $cookieStore.get('gitekplanificacion') === "1" ) {
+        $scope.uiConfig = {
+            calendar:{
+                weekNumbers: true,
+                editable: true,
+                header:{
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                dayClick: $scope.alertEventOnClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize,
+                eventSources: [
+                    {
+                        url: '/egutegia'
+                    }
+                ],
 
-    $scope.uiConfig = {
-        calendar:{
-//            height: 450,
-            weekNumbers: true,
-            editable: true,
-            header:{
-                left: 'month basicWeek basicDay agendaWeek agendaDay',
-                center: 'title',
-                right: 'today prev,next'
-            },
-            dayClick: $scope.alertEventOnClick,
-            eventDrop: $scope.alertOnDrop,
-            eventResize: $scope.alertOnResize,
-            eventSources: [
-                {
-                    url: '/egutegia'
-                }]
-        }
-    };
+                eventRender: function (event, element) {
+                    element.find('span.fc-event-title').html(element.find('span.fc-event-title').text());           
+                }
+            }
+        };
+    } else {
+        $scope.uiConfig = {
+            calendar:{
+                weekNumbers: true,
+                editable: false,
+                header:{
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                dayClick: $scope.alertEventOnClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize,
+                eventSources: [
+                    {
+                        url: '/egutegia'
+                    }
+                ]
+            }
+        };
+    }
 
     $scope.eventSources = [];
 
     $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
         var $data = {};
-        console.log(event);
-        console.log(dayDelta);
-        console.log(minuteDelta);
-        console.log(revertFunc);
-        console.log(ui);
-        console.log(view);
         $data._id = event._id;
         $data.fetxa = moment(event._start).add(dayDelta._days);
-        console.log("Hasierakoa" + moment(event.start));
-        console.log("Amaierakoa" + moment($data.fetxa));
-
         $http.put(
             '/egutegiaeguneratu', $data
         ).success(function () {
-
-            console.log("fin");
+                console.log("fin");
         });
     }
 });
