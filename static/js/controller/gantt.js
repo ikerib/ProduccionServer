828bd3939f9c14021047a6ea1a75efa58a74cf1b
Iker Ibarguren <ikerib@gmail.com>
2015-02-04 13:59:21 +0100

    It filteres but it gives some sort of error

--- static/js/controller/gantt.js
+++ static/js/controller/gantt.js
@@ -3,46 +3,6 @@
  */
 
 produccionApp.controller('ganttController', function ($scope, $http, socket, $cookieStore) {
-  // $scope.niregantt = {
-  //   data:
-  //   [
-  //     {
-  //       id:1,
-  //       text:"Project #2",
-  //       start_date:"01-04-2013",
-  //       duration:18,
-  //       order:10,
-  //       progress:0.4,
-  //       open: true
-  //     },
-  //     {
-  //       id:2,
-  //       text:"Task #1",
-  //       start_date:"02-04-2013",
-  //       duration:8,
-  //       order:10,
-  //       progress:0.6,
-  //       parent:1
-  //     },
-  //     {
-  //       id:3,
-  //       text:"Task #2",
-  //       start_date:"11-04-2013",
-  //       duration:8,
-  //       order:20,
-  //       progress:0.6,
-  //       parent:1
-  //     }
-  //   ]
-  //   ,
-  //   links:
-  //   [
-  //     { id:1, source:1, target:2, type:"1"},
-  //     { id:2, source:2, target:3, type:"0"},
-  //     { id:3, source:3, target:4, type:"0"},
-  //     { id:4, source:2, target:5, type:"2"},
-  //   ]
-  // };
 
   $scope.gantdatuak={ data: [] };
   $scope.niregantt = function () {
@@ -128,6 +88,14 @@ produccionApp.controller('ganttController', function ($scope, $http, socket, $co
     gantt.render();
 
   };
+
+  $scope.filtrarGantt = function() {
+      gantt.config.start_date = new Date(2013, 08, 10);
+      gantt.config.start_date = $scope.dtganttini;
+      gantt.config.end_date = new Date(2013, 08, 20);
+      gantt.config.end_date = $scope.dtganttfin;
+      gantt.render();
+  }
 
 
 });
